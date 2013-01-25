9df70e7680d14389d7a0b293b1bcd6e856281ee0
Clark A <clarka@gmail.com>
2013-01-24 19:13:03 -0800

    Update extractGitBranches.py
    fixes #2

--- extractGitBranches.py
+++ extractGitBranches.py
@@ -46,7 +46,7 @@ def go(options):
 
     if len(os.listdir(options.localDest))>0:
         print '--direcotry is not empty. assuming clone already done. fetching updates...'
-        cmdPull = 'git fetch'
+        cmdPull = 'git fetch -t'
         if options.showCmd: print '##command: '+cmdPull
         gitPullProc = subprocess.Popen(cmdPull,shell=True,cwd=options.localDest+options.repoRoot+'/',stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         (cmdOut,cmdErr) = gitPullProc.communicate()
